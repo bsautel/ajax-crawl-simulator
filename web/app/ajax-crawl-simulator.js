43d38ce6899d613392062ada0629b3b93525fcf0
Benoit Sautel <benoit.sautel@gmail.com>
2014-05-08 22:21:04 +0200

    First step for the angular application

--- web/app/ajax-crawl-simulator.js
+++ web/app/ajax-crawl-simulator.js
@@ -1,5 +1,37 @@
-angular.module('ajax-crawl-simulator', []);
+'use strict';
 
-function AjaxCrawlSimulatorController() {
 
+function SimulationsContoller($scope, $http) {
+    $http.get('/simulations').success(function (simulations) {
+        $scope.simulations = simulations;
+    });
+
+    $scope.view_simulation_link = function view_simulation_link(simulation) {
+        return '#' + simulationsRoute + "/" + simulation.name;
+    }
 }
+
+function SimulationController($routeParams, $scope, $http) {
+    var name = $routeParams.name;
+    var simulation_url = '/simulations/' + encodeURIComponent(name);
+    $http.get(simulation_url).success(function (simulation) {
+        $scope.simulation = simulation;
+    });
+    $http.get(simulation_url + '/pages').success(function (pages) {
+        $scope.pages = pages;
+    });
+}
+
+var simulationsRoute = '/simulations';
+var simulationRoute = simulationsRoute + '/:name';
+
+angular.module('ajax-crawl-simulator', ['ngRoute']).config(
+    [
+        '$routeProvider',
+        function ($routeProvider) {
+            $routeProvider.
+                when(simulationsRoute, {templateUrl: 'simulations.html', controller: SimulationsContoller}).
+                when(simulationRoute, {templateUrl: 'simulation.html', controller: SimulationController}).
+                otherwise({redirectTo: simulationsRoute});
+        }
+    ]);
